85ab31f3426dabe29ceaca03781b9e1bfaef8405
Octavio Perez <octavio@indetech.com.mx>
2024-01-13 21:09:15 -0600

    empezamos a modular la aplicacion, indicando los distintos endpoints con su router debido

--- index.js
+++ index.js
@@ -12,35 +12,6 @@ app.get("/nueva-ruta", (req, res) =>{
   res.send("Hola, nuevo end point de routing ");
 });
 
-app.get("/products", (req, res) =>{
-  const products = [];
-  const {size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageurl(),
-    })
-  }
-
-  res.json(products);
-});
-
-//declarar solicitudes estaticas antes de dinamicas
-app.get('/products/filter', (req,res) => {
-  res.send('Yo soy un filter');
-});
-
-//solicitud dinamica
-app.get("/products/:id", (req, res) => {
-  const { id }= req.params;
-  res.json({
-      id,
-      name: 'Product 2',
-      price: 2000
-    });
-});
 
 app.get('/users', (req, res) => {
   const { limit, offset }= req.query;
